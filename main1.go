b12b5fd9c57148796450d8400ed8ce5053f34645
JanLan <idiomasjanlan@gmail.com>
2025-01-23 21:02:38 -0300

    j

---
+++ main1.go
@@ -0,0 +1,20 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"reflect"
+// )
+
+// type object struct {
+// 	property1 string
+// 	property2 int
+// }
+
+// func main() {
+// 	z := make([]int, 10)
+// 	v := new(object)
+
+// 	fmt.Printf("La estructura de datos almacenada en z -> %v || el tipo de dato -> %v\n", z, reflect.TypeOf(z))
+// 	fmt.Printf("El puntero almacenado en v -> %v || lo que almacena la direccion en %v -> %v", v,*v, reflect.TypeOf(v))
+
+// }
